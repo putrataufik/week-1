5ff227dbbff3a5a7e663c5900c56867d5e72a772
putrataufik <daengkadaeng@gmail.com>
2023-10-17 23:23:51 +0800

    acordion menu photo profile

--- src/App.js
+++ src/App.js
@@ -1,76 +1,95 @@
-import opik from './opik1.jpg';
-import './App.css';
-import React , {useState}from 'react';
+import React, { useState } from "react";
+import opik from "./opik1.jpg";
+import "./App.css";
+
+function AccordionItem({ label, content }) {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className={`detail-item ${isActive ? 'active' : ''}`}>
+      <div className="detail-label" onClick={toggleAccordion}>
+        {label}
+        <span className={isActive ? "minus" : "plus"}>{isActive ? "-" : "+"}</span>
+      </div>
+      {isActive && <span className="detail-value">{content}</span>}
+    </div>
+  );
+}
+
 
 function App() {
   const [ubahFoto, setUbahFoto] = useState(false);
 
-  const tombolUbahFoto = () =>{
+  const tombolUbahFoto = () => {
     setUbahFoto(!ubahFoto);
-  }
-  
+  };
+
   return (
     <div className="App">
       <header className="profile">
-      <div className="profile-image">
-      {ubahFoto ? (
+        <div className="profile-image">
+          {ubahFoto ? (
             <img src={opik} className="profile-photo" alt="Foto Profil" />
-          ) : <div className="profile-photo"></div>}
+          ) : (
+            <div className="profile-photo"></div>
+          )}
           <div className="button-container">
             <button onClick={tombolUbahFoto}>Ubah Foto</button>
           </div>
-          <div className='dibawah-profile'>
+          <div className="dibawah-profile">
             <p>Gender : Male</p>
             <p>Alamat : Hertasning</p>
           </div>
         </div>
-        <div className='profile-details'>
-          <div className='name'>
+        <div className="profile-details">
+          <div className="name">
             <h1>Putra Taufik Syaharuddin</h1>
-          </div> 
-          <div className='details'>
-            <div className='detail-item'>
-              <span className='detail-label'>Hobi :</span>
-              <span className='detail-value'>Main Futsal</span>
-            </div>
-            <div className='detail-item'>
-              <span className='detail-label'>Jalur Sukses :</span>
-              <span className='detail-value'>Family Business</span>
-            </div>
-            <div className='detail-item'>
-              <span className='detail-label'>Jurusan :</span>
-              <span className='detail-value'>Informatika</span>
-            </div>
-            <div className='detail-item'>
-              <span className='detail-label'>Cita-Cita :</span>
-              <span className='detail-value'>Punya Banyak Uang</span>
-            </div>
+          </div>
+          <div className="details">
+            <AccordionItem label="Cita-Cita :" content="Banyak Uang" />
+            <AccordionItem label="Hobi :" content="Main Futsal" />
+            <AccordionItem label="Jurusan :" content="Informatika" />
+            <AccordionItem label="Tempat Tanggal Lahir :" content="Ambon 03-08-2002" />
           </div>
         </div>
       </header>
-      <div className='experience'>
+      <div className="experience">
         <h1>Organization Experience</h1>
-        <div className='experience-list'>
-          <div className='column'>
-            <div className='infinity'>
+        <div className="experience-list">
+          <div className="column">
+            <div className="infinity">
               <h3>Infinity (2022 - until death)</h3>
-              <p>Infinity adalah organisasi mahasiswa terbesar di UC Makassar. </p>
+              <p>
+                Infinity adalah organisasi mahasiswa terbesar di UC Makassar.
+              </p>
             </div>
-            <br></br>
-            <div className='oweek'>
+            <br />
+            <div className="oweek">
               <h3>O-Week 2023</h3>
-              <p>O-Week adalah kepanitiaan Orientasi Mahasiswa di UC Makassar. </p>
+              <p>
+                O-Week adalah kepanitiaan Orientasi Mahasiswa di UC Makassar.
+              </p>
             </div>
           </div>
-          <div className='column'>
-            <div className='nplc'>
+          <div className="column">
+            <div className="nplc">
               <h3>National Programming and Logic Competition 2023</h3>
-              <p>National Programming and Logic Competition 2023 adalah sebuah event yang dimana meruapakan event IMT </p>
+              <p>
+                National Programming and Logic Competition 2023 adalah sebuah
+                event yang dimana meruapakan event IMT
+              </p>
             </div>
-            <br></br>
-            <div className='Informatika Student Union'>
+            <br />
+            <div className="Informatika Student Union">
               <h3>Informatika Student Union</h3>
-              <p>Informatika Student Union merupakan organisasi mahasiswa di program studi Informatika</p>
+              <p>
+                Informatika Student Union merupakan organisasi mahasiswa di
+                program studi Informatika
+              </p>
             </div>
           </div>
         </div>
